f77d1ab2f6f6f6f16afd912ab6575a0b2473611f
vitor1klein <41600011+vitor1klein@users.noreply.github.com>
2021-01-16 17:09:54 -0300

    until "serve static files"

---
+++ hello/views.py
@@ -0,0 +1,17 @@
+import re
+from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
